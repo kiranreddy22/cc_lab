275782cfe05e289fbf734da8504dac586c09d8c3
kiranreddy22 <akrgames01@gmail.com>
2022-04-05 11:55:24 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+n=str(input("enter your name"))
+if(n=='ramya'):
+    print('your roll no is 29')
+elif(n=='puja'):
+    print('your roll no is 38')
+elif(n=='pavan'):
+    print('your roll no is 20')
+elif(n=='sai'):
+    print('your roll no is 58')
+else:
+    print('your number is not found')
